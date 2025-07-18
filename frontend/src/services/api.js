e126829e18d0e365c846b012a4632a4c0470ad41
RAHIMOVR <rehimovrustem287@gmail.com>
2025-07-18 04:02:31 +0400

    first commit

---
+++ frontend/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "e3ba9ccd4f8d3b2f88d33196d8eca0f5";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
